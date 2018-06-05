ad0ff59c6138f1b103b4648ccb00356cdd4e18e9
kushi kant <kantkushi2016@gmail.com>
2018-06-05 21:50:55 +0530

    term Project

---
+++ c2.java
@@ -0,0 +1,26 @@
+class c1
+{
+String name;
+void setName(String name)
+{
+ this.name=name;
+}
+String getName()
+{
+ return this.name;
+}
+}
+
+public class c2 extends c1
+{
+ String dep;
+void setdep(String dep)
+{
+  this.dep=dep;
+}
+ String getdep()
+{
+ return this.dep;
+}
+
+}
